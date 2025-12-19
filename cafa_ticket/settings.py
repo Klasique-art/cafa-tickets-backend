98e77ed4d12205954ed58d638ccadfe1b1832601
eritten <eritten@niveel.com>
2025-12-19 06:46:24 +0000

    update CORS settings to allow all origins

--- cafa_ticket/settings.py
+++ cafa_ticket/settings.py
@@ -186,10 +186,12 @@ SIMPLE_JWT = {
 }
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 # Payment gateway settings
